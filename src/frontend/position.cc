6ccac94193c98bad8a438a911f4de6549176e09d
Raxabi <92801687+Raxabi@users.noreply.github.com>
2025-03-26 22:59:31 +0100

    typo: fixed namespace end comment

--- src/frontend/position.cc
+++ src/frontend/position.cc
@@ -6,6 +6,6 @@ namespace Scan {
 Position::Position(int pos, int line, int pos_line):
     pos(pos), line(line), pos_line(pos_line) {}
 
-} // namespace Scrap
+} // namespace Scan
 } // namespace Scrap
 
